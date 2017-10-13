2379711f7b4f96de4b07c332825733c59ce76c1c
Anton Delsink <anton@delsink.com>
2017-10-13 14:50:46 +0400

    added Redis Client library Sider

--- src/DisruptorNetRedis_Tests/RedisClients/Client_SER_Tests.cs
+++ src/DisruptorNetRedis_Tests/RedisClients/Client_SER_Tests.cs
@@ -6,13 +6,13 @@ using System.Collections.Generic;
 using System.Net;
 using System.Text;
 
-namespace DNR.Tests.StackExchangeRedisClient
+namespace DNR.Tests.RedisClients
 {
     [TestClass]
-    public class Client_SER_Tests
+    public class Client_StackExchange_Tests
     {
         [TestMethod]
-        public void Test_SER_Connect()
+        public void Test_StackExchange_Connect_SET_GET()
         {
             var ep = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 55001);
 
---
+++ src/DisruptorNetRedis_Tests/RedisClients/Client_Sider_Tests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NFluent;
+using Sider;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace DNR.Tests.RedisClients
+{
+    [TestClass]
+    public class Client_Sider_Tests
+    {
+        [TestMethod]
+        public void Test_Sider_Connect_SET_GET()
+        {
+            var ep = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 55001);
+
+            var s = new DisruptorNetRedis.Server(ep);
+            s.Start();
+            
+            var redis = new RedisClient();
+
+            redis.Set("__key__", "__value__");
+
+            var response = redis.Get("__key__");
+            Check.That(response).IsEqualTo("__value__");
+
+            s.Stop();
+        }
+    }
+}
